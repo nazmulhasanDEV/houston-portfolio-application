250c72f3c08b50740ed281f7cdd7ad0d2ebe006d
nazmulhasan7447 <nazmulhasan747406@gmail.com>
2023-04-14 17:58:02 +0600

    added portfolio applications source code

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const linked_in_profile_link = document.getElementById('linked_in_profile_link');
+const linked_in_icon = document.getElementById('linked_in_icon');
+
+
+linked_in_icon.onclick = (event)=>{
+    linked_in_profile_link.click();
+}
